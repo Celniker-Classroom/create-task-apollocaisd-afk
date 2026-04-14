db699fc67576d47f94bbc5434f0b8e2a562368f5
apollocaisd-afk <apollocaisd@gmail.com>
2026-04-14 20:51:28 +0000

    fixed up html, began combat system, began initial messages

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// add javascript here
+let textNarration = document.getElementById("textNarration");
+let fleeChance = 70
+let playerHP = 100
+
+let enemyHPs = [1, "hello", 124]
+function combat(){
+    let userInput = document.getElementById("playerChoice");
+    let enemyHP = enemyHPs[0]
+    textNarration.textContent = "You have encountered a " + "[enemy]!\n1. Attack \n2. Defend \n3. Skills \n4. Use Item \n5. Flee (" + fleeChance + "%)";
+}
+
+function init(){
+    textNarration.textContent = "You are standing at the entrance of the castle. You hear the faint marching of castle guards inside." +
+    " The red Ethorian banner waves in the spring wind.\n1. Travel \n2. Enter Castle \n3. Rest \n4. Use Item"
+}
+
+combat();
