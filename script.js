4620059b536bb0668738e1cee04cbbf70ac4702e
apollocaisd-afk <apollocaisd@gmail.com>
2026-04-17 20:52:37 +0000

    mostly stamina system and fleeign system done, began working on inventory system, had an existential crsisi when I realized I was doing way too much than was asked

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+//CITATIONS:
+//Image "Cave Entrance" from StockCake https://stockcake.com/i/mysterious-cave-entrance_3619879_1718494
+//Image "Inside Cave" from Shutterstock: https://www.shutterstock.com/image-vector/dark-pixel-art-cave-entrance-moss-2689681071
+//Image "Crystal Cave" from Shutterstock: https://www.shutterstock.com/search/cave-pixel
+
 let textNarration = document.getElementById("textNarration");
 let fleeChance = 70
-let playerHP = 100
-let coins = 1000
+let playerHPMax = 100
+let playerHP = playerHPMax
+let staminaMax = 100
+let stamina = staminaMax
+let inventory = []
 
 let enemyHPs = [40, 100, 180]
 let enemyNames = ["Goblin", "Orc", "Troll"]
@@ -26,16 +34,40 @@ function combatLoop(){
     userInput.addEventListener("keydown", function(processInput){
         if(processInput.key === "Enter"){
             let choice = userInput.value;
+            userInput.value = "";
             eventText.textContent = "In Combat: " + enemyName;
             if(choice == "1"){
                 calculateAttack();
                 if(enemyHP <= 0){
                     eventText.textContent += "You have defeated the " + enemyName + "!";
-                    
+                    combatEnd()
                 }
                 else{
-                    enemyMove();
-                    eventText.textContent += "\n Enemy HP: " + enemyHP;
+                    enemyMove(1);
+                }
+            }
+            else if(choice == "2"){
+                eventText.textContent += "\nYou raise your shield to block the next blow. You regain some stamina.";
+                stamina += Math.floor(Math.random()* 10);
+                if (stamina > staminaMax){
+                    stamina = staminaMax;
+                }
+                enemyMove(0.5)
+            }
+            else if(choice == "3"){
+
+            }
+            else if(choice == "4"){
+
+            }
+            else if(choice == "5"){
+                roll = Math.floor(Math.random() * 100)
+                if (roll < fleeChance + 1){
+                    eventText.textContent += "\n You flee from battle.";
+                    combatEnd()
+                }
+                else{
+                    enemyMove(1);
                 }
             }
         }
@@ -45,21 +77,39 @@ function combatLoop(){
 function calculateAttack(){
     let damage = Math.floor(Math.random() * 10) + 5;
     enemyHP = enemyHP - damage;
-    eventText.textContent += "\nYou attack the " + enemyName + " for " + damage + " damage! \n";
+    eventText.textContent += "\nYou attack the " + enemyName + " for " + damage + " damage!";
     return;
 }
 
-function enemyMove(){
-    let damage = Math.floor(Math.random() * enemyDamageRanges[enemy_id]) + enemyDamageMin[enemy_id];
+function enemyMove(playerDefMultiplier){
+    let damage = Math.round((Math.floor(Math.random() * enemyDamageRanges[enemy_id]) + enemyDamageMin[enemy_id]) * playerDefMultiplier);
     playerHP = playerHP - damage;
-    eventText.textContent += "The " + enemyName + " attacks you for " + damage + " damage!";
+    eventText.textContent += "\nThe " + enemyName + " attacks you for " + damage + " damage!";
+    eventText.textContent += "\n Enemy HP: " + enemyHP;
     return;
+}
+
+function combatEnd(){
+    if (enemyName = "Cavemite"){
+        calcRewards("Cavemite Carapace", 8)
+        calcRewards("Cavemite Flesh", 8)
+    }
+    else if(enemyName = "Minion"){
+        calcRewards("Minion Meat", 6)
+        calcRewards()
+    }
+}
+
+function calcRewards(reward, chanceThreshold){
+    if (Math.floor(Math.random * 10) <= chanceThreshold){
+            inventory.push(reward);
+        }
 }
 
 function init(){
-    eventText.textContent = "You are standing at the entrance of the castle. You hear the faint marching of castle guards inside." +
-    " The red Ethorian banner waves in the spring wind."
-    choices.textContent = "\n1. Travel \n2. Enter Castle \n3. Rest \n4. Use Item"
+    eventText.textContent = "You are standing at the entrance of the cave. You hear faint groaning and creaks coming from inside." +
+    " \nYou clutch the tattered Retrieval Contract in your hand - you must retrieve the Artifact from within, lest the kingdom fall to usurpers.\n What do you do?"
+    choices.textContent = "\n1. Enter Cave"
 }
 
 combatInit();
